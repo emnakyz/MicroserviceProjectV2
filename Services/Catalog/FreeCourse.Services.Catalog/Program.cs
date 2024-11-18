f510f6c5b82f472274d115ad301230288db66f03
Emin Akyüz <emn.ak100@gmail.com>
2024-11-18 15:50:14 +0300

    GateWay-Ocelot Library 2

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,20 +1,30 @@
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog
 {
-    public class Program
+	public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+				var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+                if (!categoryService.GetAllAsync().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryDto { Name = "Test 1" }).Wait();
+					categoryService.CreateAsync(new CategoryDto { Name = "Test 2" }).Wait();
+				}
+			}
+
+            host.Run();
+		}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
