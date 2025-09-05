dd9d3e18442f61dc855631e65ca9e10fabb14e11
Emin Akyüz <emn.ak100@gmail.com>
2025-09-05 15:43:50 +0300

    IdentityService/RevokeRefreshToken

--- Frontends/FreeCourse.Web/Services/IdentityService.cs
+++ Frontends/FreeCourse.Web/Services/IdentityService.cs
@@ -96,9 +96,32 @@ namespace FreeCourse.Web.Services
 
 		}
 
-		public Task RevokeRefreshToken()
+		public async Task RevokeRefreshToken()
 		{
-			throw new System.NotImplementedException();
+			var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+			{
+				Address = _serviceApiSettings.BaseUri,
+				Policy = new DiscoveryPolicy { RequireHttps = false }
+			});
+
+			if (disco.IsError)
+			{
+				throw disco.Exception;
+			}
+
+			var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync
+									 (OpenIdConnectParameterNames.RefreshToken);
+
+			TokenRevocationRequest tokenRevocationRequest = new()
+			{
+				ClientId = _clientSettings.WebClientForUser.ClientId,
+				ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+				Token = refreshToken,
+				TokenTypeHint = "refresh_token",
+				Address = disco.RevocationEndpoint
+			};
+
+			await _httpClient.RevokeTokenAsync(tokenRevocationRequest);
 		}
 
 		public async Task<Response<bool>> SignIn(SigninInput signInInput)
