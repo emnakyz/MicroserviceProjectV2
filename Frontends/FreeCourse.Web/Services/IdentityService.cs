aebaac1e76585c83934b8b499a29c8b76a012f6d
Emin Akyüz <emn.ak100@gmail.com>
2025-09-12 19:08:18 +0300

    IUserService/UserService  GetUser - 1

--- Frontends/FreeCourse.Web/Services/IdentityService.cs
+++ Frontends/FreeCourse.Web/Services/IdentityService.cs
@@ -38,7 +38,7 @@ namespace FreeCourse.Web.Services
 		{
 			var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
 			{
-				Address = _serviceApiSettings.BaseUri,
+				Address = _serviceApiSettings.IdentityBaseUri,
 				Policy = new DiscoveryPolicy { RequireHttps = false }
 			});
 
@@ -100,7 +100,7 @@ namespace FreeCourse.Web.Services
 		{
 			var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
 			{
-				Address = _serviceApiSettings.BaseUri,
+				Address = _serviceApiSettings.IdentityBaseUri,
 				Policy = new DiscoveryPolicy { RequireHttps = false }
 			});
 
@@ -128,7 +128,7 @@ namespace FreeCourse.Web.Services
 		{
 			var disco =await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
 			{
-				Address = _serviceApiSettings.BaseUri,
+				Address = _serviceApiSettings.IdentityBaseUri,
 				Policy = new DiscoveryPolicy { RequireHttps = false }
 			});
 
