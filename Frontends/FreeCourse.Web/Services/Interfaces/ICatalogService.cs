0b5b3c4d5e60eb97ee66e51944d5dc453820a7ea
Emin Akyüz <emn.ak100@gmail.com>
2025-11-03 11:09:15 +0300

    UI-CatalogService

--- Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -6,8 +6,9 @@ namespace FreeCourse.Web.Services.Interfaces
 {
 	public interface ICatalogService
 	{
-		Task<List<CourseViewModel>> GetAllCourse();
-		Task<List<CourseViewModel>> GetAllCourseByUserId(string userId);
+		Task<List<CourseViewModel>> GetAllCourseAsync();
+		Task<List<CategoryViewModel>> GetAllCategoryAsync();
+		Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
 		Task<CourseViewModel> GetByCourseId(string courseId);
 		Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
 		Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
