39e6a126d33150c467c1122a588f1e2ac938b182
Emin Akyüz <emn.ak100@gmail.com>
2025-11-03 11:53:27 +0300

    UI-CatalogService/Methods

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -56,7 +56,7 @@ namespace FreeCourse.Services.Catalog.Controllers
 
             return CreateActionResultInstance(response);
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string id)
         {
             var response = await _courseService.DeleteAsync(id);
