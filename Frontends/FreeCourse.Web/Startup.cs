f76eb240023bbbee6a2686c25a06c7428fbe81a9
Emin Akyüz <emn.ak100@gmail.com>
2025-11-03 11:31:33 +0300

    UI-CatalogService appsettings/startup

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -34,6 +34,11 @@ namespace FreeCourse.Web
 
 			services.AddScoped<ResourceOwnerPasswordTokenHandler>();
 			services.AddHttpClient<IIdentityService,IdentityService>();
+
+			services.AddHttpClient<ICatalogService, CatalogService>(opt=>{
+				opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}");
+			});
+
 			services.AddHttpClient<IUserService, UserService>(opt =>
 			{
 				opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
