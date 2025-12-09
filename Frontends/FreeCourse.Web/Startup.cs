b058786f7324d2eae74cb55caa4360f77720a039
Emin Akyüz <emn.ak100@gmail.com>
2025-12-09 17:25:48 +0300

    CoursesController/Index Test

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -28,14 +28,15 @@ namespace FreeCourse.Web
 			services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
 			services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
 			services.AddHttpContextAccessor();
+			services.AddAccessTokenManagement();
 			services.AddScoped<ISharedIdentityService, SharedIdentityService>();
 			var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 
-			services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
 			services.AddScoped<ResourceOwnerPasswordTokenHandler>();
 			services.AddScoped<ClientCredentialTokenHandler>();
-			services.AddHttpClient<IIdentityService,IdentityService>();
 
+			services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
+			services.AddHttpClient<IIdentityService,IdentityService>();
 			services.AddHttpClient<ICatalogService, CatalogService>(opt =>
 			{
 				opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}");
